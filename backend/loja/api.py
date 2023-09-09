76c7217e939c2440cbcd609a963aad70f6c9ab6f
Marcos <devmarcos.py@gmail.com>
2023-09-09 18:16:59 -0300

    adicionado listagem  de outros produtos e de todos

--- backend/loja/api.py
+++ backend/loja/api.py
@@ -1,8 +1,8 @@
 from typing import List
 from ninja import NinjaAPI
 
-from backend.loja.schemas import CamisasSchema
-from backend.loja.models import Camisetas
+from backend.loja.schemas import CamisasSchema, CalcasSchema, BlusasSchema
+from backend.loja.models import Camisetas, Blusas, Calcas
 
 api = NinjaAPI()
 
@@ -10,3 +10,43 @@ api = NinjaAPI()
 def camisetas(request):
     camisetas = Camisetas.objects.all()
     return camisetas
+
+@api.get("/blusas", response=List[BlusasSchema])
+def blusas(request):
+    blusas = Blusas.objects.all()
+    return blusas
+
+@api.get("/calcas", response=List[CalcasSchema])
+def calcas(request):
+    calcas = Calcas.objects.all()
+    return calcas
+
+
+@api.get("/todos_produtos")
+def todos_produtos(request):
+    data = {}
+    excluded_fields = ("id", "created_at", "updated_at")
+    calcas = Calcas.objects.all().values()
+    blusas = Blusas.objects.all().defer(excluded_fields).values()
+    camisetas = Camisetas.objects.all().defer(excluded_fields).values()
+
+    data["blusas"] = []
+    data["camisetas"] = []
+    data["calcas"] = []
+
+    for item in camisetas:
+        for field in excluded_fields:
+            item.pop(field, None)
+        data["camisetas"] += [item]
+    
+    for item in blusas:
+        for field in excluded_fields:
+            item.pop(field, None)
+        data["blusas"] += [item]
+
+    for item in calcas:
+        for field in excluded_fields:
+            item.pop(field, None)
+        data["calcas"] += [item]
+
+    return data
