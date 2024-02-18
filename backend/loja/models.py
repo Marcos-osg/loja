ee4043e40856fb15f0dfe0379944f119beec6b3e
Marcos <devmarcos.py@gmail.com>
2024-02-18 18:05:53 -0300

    refactor: alterado models, preparando para mais atualizações

--- backend/loja/models.py
+++ backend/loja/models.py
@@ -18,6 +18,7 @@ class Header(models.Model):
         return f"{self.id}, {self.created_at}"
 
 class Produtos(Header):
+    """ Generico para roupas """
     tipo = models.CharField(max_length=64, choices=choices.tipo)
     cor = models.CharField(max_length=64, choices=choices.lista_de_cores)
     tamanho = models.CharField(max_length=64, choices=choices.tamanhos_camisetas)
@@ -30,9 +31,19 @@ class Produtos(Header):
     class Meta:
         verbose_name = "Produto"
         verbose_name_plural = "Produtos"
+        
+    def _get_price_product(self):
+        """ Retorna o preço do produto sendo ele promocional ou não """
+        if self.preco_promo:
+            return self.preco_promo
+        else:
+            return self.preco
+    
+    def _name_product(self):
+        return f"{self.tipo} {self.marca} {self.tamanho} {self.cor}"
 
     def __str__(self):
-        return f"{self.tipo} {self.marca} - {self.tamanho} - {self.cor} -- R${self.preco}"
+        return f"{self.tipo} {self.marca} - {self.tamanho} - {self.cor} -- R${self._get_price_product()}"
     
 
 class Carrinho(Header):
@@ -42,6 +53,19 @@ class Carrinho(Header):
     
     def save(self, *args, **kwargs):
         return super(Carrinho, self).save(*args, **kwargs)
+    
+    def _get_total_cart(self):
+        valor_final = 0
+        itens_carrinho = ItensCarrinho.objects.filter(carrinho=self)
+        for item in itens_carrinho:
+            valor_final += item.valor_total_produto
+        
+        """ salva o valor final no carrinho """
+        self.valor_final = valor_final
+        self.save()
+        
+        """ retorna o valor final para uso no front """
+        return valor_final
 
     class Meta:
         verbose_name = "Carrinho de compra"
@@ -53,27 +77,20 @@ class Carrinho(Header):
 
 class ItensCarrinho(Header):
     produto = models.ForeignKey(Produtos, on_delete=models.DO_NOTHING)
-    quantidade = models.PositiveIntegerField()
-    valor_produtos = models.DecimalField(max_digits=10, decimal_places=2 ,default=0)
+    quantidade = models.PositiveIntegerField(default=1)
+    valor_unitario = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    valor_total_produto = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     carrinho = models.ForeignKey(Carrinho, on_delete=models.DO_NOTHING)
     
     def save(self, *args, **kwargs):
-        self.valor_produtos = self.soma_produtos()
-        self.ajusta_estoque()
+        valor_unitario = self.produto._get_price_product()
+        self.valor_unitario = valor_unitario
+        self.valor_total_produto = self.quantidade * valor_unitario
         return super(ItensCarrinho, self).save(*args, **kwargs)
-    
-    def soma_produtos(self):
-        valor_produtos = self.produto.preco * self.quantidade
-        return valor_produtos
-
-    def ajusta_estoque(self):
-        estoque = self.produto.estoque - self.quantidade
-        self.produto.estoque = estoque
-        self.produto.save()
 
     class Meta:
         verbose_name = "Item do Carrinho"
         verbose_name_plural = "Itens do Carrinho"
 
     def __str__(self):
-        return f"{self.produto} x {self.quantidade}"
+        return f"{self.produto} x {self.quantidade} - R$ {self.valor_total_produto}"
