a6430aad163b327a03b1edc94b059b14dab3c142
Marcos <devmarcos.py@gmail.com>
2024-02-18 18:06:30 -0300

    docs: adicionado steps da loja

---
+++ backend/loja/steps.py
@@ -0,0 +1,35 @@
+""" 
+    PRODUTOS 
+    
+    - cadastrar produtos
+    - listar produtos
+    - editar produtos
+    - excluir produtos
+    - controle de estoque
+
+
+    CARRINHO DE COMPRAS - model
+    - pedido
+    - valor final do carrinho
+    - pago (boolean)
+    
+    CARRINHO DE COMPRAS - funções
+    
+    - criar carrinho de compras caso não exista, se existir pegar o existente de acordo com o usuario = ok
+    - adicionar ao carrinho = ok
+    - remover do carrinho = ok
+    - listar todos os itens do carrinho = ok
+    - fechar pedido (trancar para prosseguir com o pagamento)
+
+
+    ITENS DO CARRINHO - model
+    - produto
+    - quantidade
+    - preço unitario
+    - preço total
+
+
+    PEDIDO
+
+    - após finalizar o carrinho criar um pedido com a referencia do carrinho e seus itens
+"""
